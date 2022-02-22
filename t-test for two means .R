81290bad44414dab70b3b230690a72bbec6f3909
YAMAGIRI SALEEMBASHA <95207243+yamagirisaleembasha@users.noreply.github.com>
2022-02-22 11:40:56 +0530

    9 b

--- t-test for two means .R
+++ t-test for two means .R
@@ -1,47 +1,42 @@
-#F-test for SST
-x <- c(9,11,13,11,15,9,12,14)
+#t-test for two means SST (n1!=n2)
+x <- c(8260,8130,8350,8070,8340)
 n1 <- length(x)
-y <- c(10,12,10,14,9,8,10)
+y <- c(7950,7890,7900,8140,7920,7840)
 n2 <- length(y)
 los <- 0.01
-fbuiltin <- var.test(x,y)
-meanx <- sum(x)/n1
-meany <- sum(y)/n2
-sxsquare <-(sum((x-meanx)**2))/(n1-1)
-print(sxsquare)
-sysquare <- (sum((y-meany)**2))/(n2-1)
-print(sysquare)
-fcal <- sxsquare/sysquare
-if (fcal<1)
+meanx <- mean(x)
+print(meanx)
+meany <- mean(y)
+print(meany)
+s <- sqrt((sum((x-meanx)**2)+sum((y-meany)**2))/(n1+n2-2))
+print(s)
+tcal <- (meanx-meany)/(s*sqrt((1/n1)+(1/n2)))
+tc <- t.test(x,y)#built in method to calculate tcal in t test
+print(tc)
+print(tcal)
+ttable <- qt(1-(los/2),df=n1+n2-2)
+print(ttable)
+if(ttable>tcal)
 {
-  fcal <- 1/f
-  print(fcal)
-  ftable <- qf(1-los,n2-1,n1-1)
-  print(ftable)
+  print("difference between the means of these samples are not significant")
 }else{
-  print(fcal)
-  ftable <- qf(1-los,n1-1,n2-1)
-  print(ftable)
+  print("difference between the means of these samples are significant")
 }
-if(ftable<fcal)
-{
-  print("null hypothesis rejected and alternative hypothes is accepted")
-}else{
-  print("null hypothesis accepted")
-}
-#F-test for LST
+#t-test for two means LST for both (n1!=n2) and (n1==n2)
 n1 <- 40
-sd1 <- 34
-n2 <- 60
-sd2 <- 28
+n2 <- 40
+mean1 <- 647
+mean2 <- 638
+sd1 <- 27
+sd2 <- 31
 los <- 0.05
-zcal <- (sd1-sd2)/(sqrt(((sd1^2)/(2*n1))+((sd2^2)/(2*n2))))
+zcal <- (mean1-mean2)/sqrt((sd1**2/n1)+(sd2**2/n2))
 print(zcal)
-zcritical <- qnorm(1-(los/2))
-print(zcritical)
-if(zcritical<zcal)
+zcritical <- qnorm(1-los) #for left tailed test write qnorm(confindence limits,lower.tail=true)-to get zcritical value
+print(zcritical)          #for both two and right tailed test write qnorm(confindence limits)-to get zcritical value
+if(zcritical>zcal)
 {
-  print("null hypothesis rejected and alternative hypothes is accepted")
+  print("first sample is equal to second sample")
 }else{
-  print("null hypothesis accepted")
+  print("first sample is suferior to second sample")
 }
