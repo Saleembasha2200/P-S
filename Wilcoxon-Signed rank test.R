14be176a1750ef9d797458d9efa86e9fecb9b55c
YAMAGIRI SALEEMBASHA <95207243+yamagirisaleembasha@users.noreply.github.com>
2022-02-23 09:36:49 +0530

    11 b

---
+++ Wilcoxon-Signed rank test.R
@@ -0,0 +1,26 @@
+print("wilcoxon signed rank test for one sample :")
+print("enter observations:")
+x <- scan()
+print("enter mean or median value:")
+muew <- scan()
+print("enter level of significance:")
+los <- scan()
+t <- wilcox.test(x,mu=muew,alternative="two.sided")
+if (t$p.value>los){
+  print("accept Ho")
+}else{
+  print("reject Ho") 
+}
+print("wilcoxon signed rank test for (paired sample test ) n1==n2:")
+print("enter first observations:")
+x <- scan()
+print("enter second observations:")
+y <- scan()
+print("enter level of significance:")
+los <- scan()
+t <- wilcox.test(x,y,paired=TRUE,mu=muew,alternative="two.sided")
+if (t$p.value>los){
+  print("accept Ho")
+}else{
+  print("reject Ho") 
+}
