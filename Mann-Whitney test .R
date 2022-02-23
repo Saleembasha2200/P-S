e5ddb23a4e72a01a002224d036e0c77df90ca286
YAMAGIRI SALEEMBASHA <95207243+yamagirisaleembasha@users.noreply.github.com>
2022-02-23 10:02:33 +0530

    12 a

---
+++ Mann-Whitney test .R
@@ -0,0 +1,13 @@
+print("mann-witney test for n1!=n2")
+print("enter first observations")
+x <- scan()
+print("enter second observations")
+y <- scan()
+print("enter level of significance")
+los <- scan()
+t <- wilcox.test(x,y,alternative="two.sided")
+if (t$p.value>los){
+  print("accept Ho")
+}else{
+  print("reject Ho") 
+}
